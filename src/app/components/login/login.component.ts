817393654f1eb441bd28e86fb950e321e18bffbe
Hicham Guendouz <Hicham.guendouz@gmail.com>
2023-03-09 17:35:52 +0100

    Construction du login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login/login.service';
+import { AppSettings } from '../../settings/app.settings';
+
+interface loginUser{
+  username: string,
+  password: string
+}
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  constructor(private loginService: LoginService) {
+  }
+  ngOnInit(): void {
+    this.
+    this.onLogin();
+    console.log('je suis dans logincomponent');
+
+
+  }
+
+  public onLogin() {
+    this.loginService.login().subscribe(
+      (data:any) => {
+        console.log(data);
+
+      })
+
+  }
+}
